5e00ea699bd3d2f29ea7882240a09074ea526e01
smartpolitech <smartpolitech@unex.es>
2016-04-06 19:14:12 +0200

    cambios en avatar

--- aiml/aiml.py
+++ aiml/aiml.py
@@ -2,17 +2,20 @@ import sys
 sys.path.insert(0, "PyAIML")
 
 import aiml
+#import os
 
 # The Kernel object is the public interface to
 # the AIML interpreter.
-k = aiml.Kernel()
+kernel = aiml.Kernel()
 
 # Use the 'learn' method to load the contents
 # of an AIML file into the Kernel.
-k.learn("std-startup.xml")
+kernel.learn("inicial.xml")
 
-k.respond("LOAD AIML B")
+
+kernel.respond("LOAD AIML B")
 
 # Loop forever, reading user input from the command
 # line and printing responses.
-while True: print k.respond(raw_input("> "))
+while True: 
+	print kernel.respond(raw_input("HABLAME...> "))
