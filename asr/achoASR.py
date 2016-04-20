06250d1738e386bcb70a63deeeb5dc8ce39a0a49
santyVargas <savargasm@alumnos.unex.es>
2016-04-20 18:59:51 +0200

    Update achoASR.py

--- asr/achoASR.py
+++ asr/achoASR.py
@@ -10,7 +10,7 @@ def on_connect(client, userdata, flags, rc):
 
     # Subscribing in on_connect() means that if we lose the connection and
     # reconnect then subscriptions will be renewed.
-    client.subscribe("acho/tasr/#")
+    client.subscribe("acho/asr/#")
 
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
